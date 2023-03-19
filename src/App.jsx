de5023124fa4888aa38f76fb99a82dfe4f7e2a9d
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-03-19 18:54:11 +0000

    progress

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { ShopContextProvider } from "./context/shop-context";
+import { Cart } from "./pages/cart/Cart";
+import Shop from "./pages/shop/Shop";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
